2b8c32505ed5546390c34a1144447af31b9c65ad
Vignesh Anil <72751276+vzzyxn@users.noreply.github.com>
2021-11-07 18:06:25 +0530

    Add files via upload

---
+++ python/guessnumber.py.py
@@ -0,0 +1,26 @@
+# python number guessing game
+guess_num = 9
+guess_limit = 3
+guess_count = 0
+print('You have three chances to guess the correct number')
+while guess_limit > guess_count:
+    guess_count += 1
+    user_input = int(input('enter number:'))
+    if user_input == guess_num:
+        print('Correct Number!')
+        break
+    else:
+        print('Wrong Number!')
+
+
+
+
+
+
+
+
+
+
+
+
+
