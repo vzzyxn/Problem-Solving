bed42cf1b16825b2a2871978cb79fa0a4d2c559b
Vignesh A <72751276+vzzyxn@users.noreply.github.com>
2022-04-08 22:45:18 +0530

    Update watermelon.cpp

--- C++/watermelon.cpp
+++ C++/watermelon.cpp
@@ -5,6 +5,10 @@ using namespace std;
 int main() {
     int w;
     cin >> w;
+    // Algorithm = Brute force
+    // 1.find if w(weight) == even number 
+    // 2.find w can be divide into to equal parts 
+    //      >> if correct YES >>else NO
     if(w % 2 == 0 && w > 2) {
         cout << "YES";
     }
@@ -13,4 +17,3 @@ int main() {
     }
     return 0;
 }
-
