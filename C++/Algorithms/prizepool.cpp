6bf6245897cdd926db7dfb4d0cf8b393b1f4b707
Vignesh A <72751276+vzzyxn@users.noreply.github.com>
2022-05-25 21:21:11 +0530

    Add files via upload

---
+++ C++/Algorithms/prizepool.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+		int t;
+		cin >> t;
+		for(int i = 1; i <=t; i++) {
+			int x, y, top_ten, top_ninety, total;
+			cin >> x >> y;q
+			top_ten = (x * 10 );
+			top_ninety = (y * 90);
+			total = top_ten + top_ninety;
+			cout << total << "\n";
+		}
+			
+	return 0;
+}
